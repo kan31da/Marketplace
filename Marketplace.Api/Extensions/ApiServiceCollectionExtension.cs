f96cacc645051e4e8a648f455a6ea575af4c7081
Vladislav Vladovski <garbage@mail.bg>
2022-03-24 13:56:39 +0200

    add api

---
+++ Marketplace.Api/Extensions/ApiServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Marketplace.Infrastructure.Data;
+using Marketplace.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ApiServiceCollectionExtension
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicatioDbRepository, ApplicatioDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApiDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
