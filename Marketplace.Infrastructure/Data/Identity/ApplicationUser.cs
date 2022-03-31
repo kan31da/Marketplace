335225141bace2fa836ec0f6f8ed8a0521d444fc
Vladislav Vladovski <garbage@mail.bg>
2022-03-31 12:07:15 +0300

    update register

--- Marketplace.Infrastructure/Data/Identity/ApplicationUser.cs
+++ Marketplace.Infrastructure/Data/Identity/ApplicationUser.cs
@@ -8,10 +8,22 @@ namespace Marketplace.Infrastructure.Data.Identity
     public class ApplicationUser : IdentityUser
     {
         [StringLength(ModelConstants.FIRSTNAME_LENTGH)]
-        public string? FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter FirstName")]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
 
         [StringLength(ModelConstants.LASTNAME_LENTGH)]
-        public string? LastName { get; set; }
+        [Required(ErrorMessage = "Please enter LastName")]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        [Required(ErrorMessage = "Phone Number Required!")]
+        [RegularExpression(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$", ErrorMessage = "Entered phone format is not valid.")]
+        public string PhoneNumber { get; set; }
+
+        public Nullable<bool> Is_Deleted { get; set; }
 
         [Required]
         public Cart Cart { get; set; } = new Cart();
