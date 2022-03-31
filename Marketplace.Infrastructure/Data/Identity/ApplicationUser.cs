51283a9e0f612d99e2ff33635e1db513761b0b9d
Vladislav Vladovski <garbage@mail.bg>
2022-03-31 12:15:51 +0300

    datamodel user update

--- Marketplace.Infrastructure/Data/Identity/ApplicationUser.cs
+++ Marketplace.Infrastructure/Data/Identity/ApplicationUser.cs
@@ -11,22 +11,28 @@ namespace Marketplace.Infrastructure.Data.Identity
         [Required(ErrorMessage = "Please enter FirstName")]
         [Display(Name = "FirstName")]
         public string FirstName { get; set; }
+        //
 
         [StringLength(ModelConstants.LASTNAME_LENTGH)]
         [Required(ErrorMessage = "Please enter LastName")]
         [Display(Name = "LastName")]
         public string LastName { get; set; }
+        //
 
-        [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Phone Number")]
-        [Required(ErrorMessage = "Phone Number Required!")]
-        [RegularExpression(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$", ErrorMessage = "Entered phone format is not valid.")]
-        public string PhoneNumber { get; set; }
+        //[StringLength(ModelConstants.PHONENUMBER_LENTGH)]
+        //[DataType(DataType.PhoneNumber)]
+        //[Display(Name = "Phone Number")]
+        //[Required(ErrorMessage = "Phone Number Required!")]
+        //[RegularExpression(@"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$", ErrorMessage = "Entered phone format is not valid.")]
+        //public string PhoneNumber { get; set; }
+        //
 
         public Nullable<bool> Is_Deleted { get; set; }
+        //
 
         [Required]
         public Cart Cart { get; set; } = new Cart();
+        //
 
         public ICollection<Order> Orders { get; set; } = new List<Order>();
     }
---
+++ Marketplace.Infrastructure/Migrations/20220331091427_userupdate.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marketplace.Infrastructure.Data.Migrations
+{
+    public partial class userupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Is_Deleted",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Is_Deleted",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
