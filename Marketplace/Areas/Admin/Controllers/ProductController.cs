88f0213292e5c716e068cd58e931bd8a442d0f96
Vladislav Vladovski <garbage@mail.bg>
2022-04-05 20:08:42 +0300

    produtccontroller update

---
+++ Marketplace/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Areas.Admin.Controllers
+{
+    public class ProductController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ManageProducts()
+        {
+            return View();
+        }
+    }
+}
